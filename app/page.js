457ede8238f9571b9af771146c3a9c50ba98b311
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-31 18:07:59 +0100

    Add footer items to menu

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence, delay } from "framer-motion"
 
 export default function Home() {
   const [isOpened, setIsOpened] = useState(false)
@@ -57,11 +57,27 @@ export default function Home() {
     })
   }
 
+  const details = {
+    initial: {
+      opacity: 0,
+      y: 20,
+    },
+    enter: {
+      opacity: 1,
+      y: 0,
+      transition: {delay: 0.9, duration: 0.6},
+    },
+    exit: {
+      y: 20,
+      opacity: 0,
+    }
+  }
+
   return (
     <main>
       <AnimatePresence mode="wait">
         {!isOpened ? (
-          <div className="flex min-h-screen p-12">
+          <div className="flex min-h-screen p-8">
             <motion.div key="hamburger" onClick={handleClick} className="flex flex-col ml-auto gap-y-1.5 hover:cursor-pointer p-2">
               <motion.span 
                 initial={{transform: "rotate(45deg)", origin: "center"}} 
@@ -83,7 +99,7 @@ export default function Home() {
             animate="enter" 
             exit="exit" 
             className="bg-black">
-            <div className="relative flex flex-col p-12 h-full">
+            <div className="relative flex flex-col p-8 h-full">
               <div onClick={handleClick} className="relative flex flex-col ml-auto gap-y-1.5 hover:cursor-pointer p-2 z-10">
                 <motion.span 
                   initial={{transform: "rotate(0deg)"}} 
@@ -96,7 +112,7 @@ export default function Home() {
                   className="w-7 h-0.5 bg-white">
                 </motion.span>
               </div>
-              <ul className="absolute mt-44 inset-0 text-center z-0">
+              <ul className="relative z-0">
                   {items.map((item, i) => (
                     <li key={`l_${i}`} className="hover:cursor-pointer overflow-hidden pb-4">
                       <motion.span
@@ -106,12 +122,38 @@ export default function Home() {
                         animate="enter" 
                         exit="exit"
                         key={i} 
-                        className="flex flex-col uppercase text-6xl leading-relaxed font-extralight text-white">
+                        className="flex flex-col uppercase text-6xl leading-relaxed font-extralight text-neutral-300">
                         {item}
                       </motion.span>
                     </li>
                   ))}
               </ul> 
+              <div key="details" className="flex flex-row justify-between mt-auto">
+                <motion.span 
+                  variants={details} 
+                  initial="initial" 
+                  animate="enter" 
+                  exit="exit" 
+                  className="uppercase text-sm text-neutral-600 overflow-hidden">
+                    Made by: <span className="text-neutral-400">Fernanda Freitas</span>
+                </motion.span>
+                <motion.span 
+                  variants={details} 
+                  initial="initial" 
+                  animate="enter" 
+                  exit="exit" 
+                  className="uppercase text-sm text-neutral-600 overflow-hidden">
+                    Typography: <span className="text-neutral-400">Google Fonts</span>
+                </motion.span>
+                <motion.span 
+                  variants={details} 
+                  initial="initial" 
+                  animate="enter" 
+                  exit="exit" 
+                  className="uppercase text-sm text-neutral-400 overflow-hidden hover:cursor-pointer">
+                    Privacy Policy | Terms and conditions
+                </motion.span>
+              </div>
             </div>
           </motion.div>
         )}
