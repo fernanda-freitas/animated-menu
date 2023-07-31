b39195b43faeb68eb29623dc7762e2b8e06e50fe
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-31 09:45:19 +0100

    Add internal menu items

--- app/page.js
+++ app/page.js
@@ -20,11 +20,17 @@ export default function Home() {
           </div>   
         </div>
       ) : (
-        <div className="flex min-h-screen p-12 bg-black">
+        <div className="flex flex-col min-h-screen p-12 bg-black">
           <div onClick={handleClick} className="flex flex-col ml-auto gap-y-1.5 hover:cursor-pointer">
             <span className="w-7 h-0.5 bg-white origin-center rotate-45 translate-y-1"></span>
             <span className="w-7 h-0.5 bg-white origin-center -rotate-45 -translate-y-1"></span>
-          </div>   
+          </div>
+          <ul className="m-auto text-center">
+            <li className="text-6xl leading-relaxed font-extralight text-white hover:cursor-pointer">Homepage</li>
+            <li className="text-6xl leading-relaxed font-extralight text-white hover:cursor-pointer">About</li>
+            <li className="text-6xl leading-relaxed font-extralight text-white hover:cursor-pointer">Works</li>
+            <li className="text-6xl leading-relaxed font-extralight text-white hover:cursor-pointer">Contact</li>
+          </ul>   
         </div>
       )}
     </main>
